28ccc2a10767dd5460578c5364604610f364ddec
Nikunj-TST <nikunj@tsttechnology.io>
2025-03-24 16:44:55 +0530

    [Fix] Minor fixes

--- test/animated_toast_demo_test.dart
+++ test/animated_toast_demo_test.dart
@@ -1,6 +1,5 @@
+import 'package:animated_toast/animated_toast_demo.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:animated_toast_demo/animated_toast_demo.dart';
 
 void main() {
   test('adds one to input values', () {
